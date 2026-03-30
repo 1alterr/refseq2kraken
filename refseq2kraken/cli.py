85547f89bd35775b6776969d3d03b2bbb3175499
1alterr <125842397+1alterr@users.noreply.github.com>
2026-03-30 16:16:17 -0300

    update README with project description

--- refseq2kraken/cli.py
+++ refseq2kraken/cli.py
@@ -1,30 +1,57 @@
-import typer
+import argparse
+import glob
+
 from refseq2kraken.download import download_pipeline
-from refseq2kraken.kraken import add_to_library, build_db
+from refseq2kraken.kraken import init_db, add_to_library, build_db
+from refseq2kraken.utils import ensure_dir
 
-app = typer.Typer()
 
-@app.command()
-def download(
-    group: str = typer.Option(..., help="refseq group (plant, invertebrate, etc)"),
-    threads: int = 8,
-    outdir: str = "data"
-):
-    download_pipeline(group, threads, outdir)
+def main():
+    parser = argparse.ArgumentParser(
+        prog="refseq2kraken",
+        description="Download Taxonomy → RefSeq → Kraken2 Build",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
 
-@app.command()
-def add(
-    input_dir: str,
-    db: str
-):
-    add_to_library(input_dir, db)
+    parser.add_argument("--group", required=True, help="RefSeq group (ex: plant, fungi)")
+    parser.add_argument("--db", required=True, help="Kraken DB path")
+    parser.add_argument("--outdir", required=True, help="Output directory")
 
-@app.command()
-def build(
-    db: str,
-    threads: int = 16
-):
-    build_db(db, threads)
+    parser.add_argument("--download-threads", type=int, default=6)
+    parser.add_argument("--build-threads", type=int, default=32)
+
+    parser.add_argument("--skip-init", action="store_true")
+    parser.add_argument("--skip-download", action="store_true")
+    parser.add_argument("--skip-add", action="store_true")
+    parser.add_argument("--skip-build", action="store_true")
+
+    args = parser.parse_args()
+
+    ensure_dir(args.outdir)
+
+    fna_files = []
+
+    if not args.skip_init:
+        init_db(args.db)
+
+    if not args.skip_download:
+        fna_files = download_pipeline(
+            args.group,
+            args.download_threads,
+            args.outdir
+        )
+
+    if not args.skip_add:
+        if not fna_files:
+            fna_files = glob.glob(f"{args.outdir}/*.fna")
+
+        add_to_library(fna_files, args.db)
+
+    if not args.skip_build:
+        build_db(args.db, args.build_threads)
+
+    print("[DONE]")
+
 
 if __name__ == "__main__":
-    app()
+    main()
